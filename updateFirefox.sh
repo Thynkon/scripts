551d6831984e51685d8ed0a20ac44a5a569b7d4c
Mário Ferreira <thynkon@protonmail.com>
2020-08-27 09:43:25 +0200

    Download new version of firefox if it is not already on the download directory

--- updateFirefox.sh
+++ updateFirefox.sh
@@ -1,11 +1,41 @@
 #!/usr/bin/env sh
 
-cd /tmp
-curl --location "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US"  | tar --extract --verbose --preserve-permissions --bzip2
+DOWNLOAD_DIRECTORY="/tmp"
+FIREFOX_DIRECTORY="/opt/firefox"
+CURRENT_FIREFOX_VERSION=$(grep "BuildID" "${FIREFOX_DIRECTORY}/application.ini" | cut -d '=' -f 2)
 
-if [[ -d /opt/firefox ]]; then
-	rm -r /opt/firefox
-fi
+main() {
+	if [[ $EUID -ne 0 ]]; then
+		echo "You must run this script as root!"
+		echo "Exiting..."
+		exit 1
+	fi
 
-mv /tmp/firefox /opt/firefox
-chown -R root:root /opt/firefox
+	if [[ -f "${DOWNLOAD_DIRECTORY}/firefox/application.ini" ]]; then
+		downloaded_firefox_version=$(grep "BuildID" "${DOWNLOAD_DIRECTORY}/firefox/application.ini" | cut -d '=' -f 2)
+		if [[ $downloaded_firefox_version > $CURRENT_FIREFOX_VERSION ]]; then
+			mv "${DOWNLOAD_DIRECTORY}/firefox" "${FIREFOX_DIRECTORY}"
+			chown -R root:root "${FIREFOX_DIRECTORY}"
+			echo "USING DOWNLOADED VERSION IN /TMP/FIREFOX"
+			exit 0
+		else
+			echo "Deleting old firefox version"
+			rm -r "${DOWNLOAD_DIRECTORY}/firefox"
+		fi
+	fi
+
+	# stop any running instances of firefox
+	pkill -f "firefox*"
+
+	cd "${DOWNLOAD_DIRECTORY}"
+	curl --location "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US"  | tar --extract --verbose --preserve-permissions --bzip2
+
+	if [[ -d "${FIREFOX_DIRECTORY}" ]]; then
+		rm -r "${FIREFOX_DIRECTORY}"
+	fi
+
+	mv "${DOWNLOAD_DIRECTORY}/firefox" "${FIREFOX_DIRECTORY}"
+	chown -R root:root "${FIREFOX_DIRECTORY}"
+}
+
+main $@
